7ff887f774c9252ddc0a78a040817110faa4ed4c
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2024-01-02 14:09:02 +0300

    Add files via upload

---
+++ TASİT UYGULAMASI-1473/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TASİT_UYGULAMASI_1473
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.txtFıyat = new System.Windows.Forms.TextBox();
+            this.lbTaksıt = new System.Windows.Forms.ListBox();
+            this.lbTarih = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(28, 137);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(168, 60);
+            this.btnHesapla.TabIndex = 0;
+            this.btnHesapla.Text = "HESAPLA";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // txtFıyat
+            // 
+            this.txtFıyat.Location = new System.Drawing.Point(28, 52);
+            this.txtFıyat.Name = "txtFıyat";
+            this.txtFıyat.Size = new System.Drawing.Size(168, 20);
+            this.txtFıyat.TabIndex = 1;
+            this.txtFıyat.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lbTaksıt
+            // 
+            this.lbTaksıt.FormattingEnabled = true;
+            this.lbTaksıt.Location = new System.Drawing.Point(234, 65);
+            this.lbTaksıt.Name = "lbTaksıt";
+            this.lbTaksıt.Size = new System.Drawing.Size(120, 212);
+            this.lbTaksıt.TabIndex = 2;
+            // 
+            // lbTarih
+            // 
+            this.lbTarih.FormattingEnabled = true;
+            this.lbTarih.Location = new System.Drawing.Point(409, 65);
+            this.lbTarih.Name = "lbTarih";
+            this.lbTarih.Size = new System.Drawing.Size(120, 212);
+            this.lbTarih.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(28, 90);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(168, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.lbTarih);
+            this.Controls.Add(this.lbTaksıt);
+            this.Controls.Add(this.txtFıyat);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.TextBox txtFıyat;
+        private System.Windows.Forms.ListBox lbTaksıt;
+        private System.Windows.Forms.ListBox lbTarih;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ TASİT UYGULAMASI-1473/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TASİT_UYGULAMASI_1473
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double taksit = Convert.ToInt32(txtFıyat.Text) / Convert.ToDouble(numericUpDown1.Value);
+
+            DateTime tarih = DateTime.Now;
+            lbTaksıt.Items.Clear();
+            for (int i = 1; i <= numericUpDown1.Value; i++)
+            {
+                lbTaksıt.Items.Add($" {i} taksit: {taksit} ");
+
+                DateTime yeniTarih = DateTime.Now;
+
+                lbTarih.Items.Add($"Yıl: {yeniTarih.Year} Ay: {yeniTarih.Month}. Gün: {yeniTarih.Day} ");
+
+            }
+        }
+    }
+}   
